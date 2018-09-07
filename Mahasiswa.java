f98397c29a9b126284224515d851a59249e1d416
mona.natasha <mona_natasha@ymail.com>
2018-09-07 18:44:33 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ * @author Mona Natasha
+ * 1606874601
+ * APAP - C
+ *
+ */
+
+public class Mahasiswa {
+	public static float ipk;
+	public static String nama;
+	public static String npm;
+	
+	public Mahasiswa() {
+		this.ipk = ipk;
+		this.nama = nama;
+		this.npm = npm;
+	}
+
+	public static float getIpk() {
+		return ipk;
+	}
+
+	public static void setIpk(float ipk) {
+		Mahasiswa.ipk = ipk;
+	}
+
+	public static String getNama() {
+		return nama;
+	}
+
+	public static void setNama(String nama) {
+		Mahasiswa.nama = nama;
+	}
+
+	public static String getNpm() {
+		return npm;
+	}
+
+	public static void setNpm(String npm) {
+		Mahasiswa.npm = npm;
+	}
+}
